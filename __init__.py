7cd8ae42a6ab051ffff37175c77188ebb863ae5d
Ray Hou <songlinhou1993@gmail.com>
2021-06-17 03:30:41 +0000

    bug fix

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from ._utils import user_select, try_mount_drive, setting_workspace_drive, setti
 class Code(object):
     def init_env(self, mount_path = '/content/drive', regen_token = False, silent = True):
         from .dev.code.codeapp import download_vscode
-        from ._utils import install_pip_dependencies
+        from ._utils import install_pip_dependencies, install_bash_dependencies
         if not os.path.exists(mount_path):
             choice = user_select("Mount your google drive?", ["Yes","No"])
             if choice == 1:
@@ -13,7 +13,8 @@ class Code(object):
         setting_up_caches(silent)
         download_vscode(silent)
         load_token(regen_token)
-        install_pip_dependencies()
+        install_pip_dependencies(silent)
+        install_bash_dependencies(silent)
 
     def start_server(self):
         from .dev.code.codeapp import start_vscode_loop
